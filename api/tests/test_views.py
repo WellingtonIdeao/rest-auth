fa51369952bea2df816c09af1245002f31bdeab0
Wellington Ideão <wellington.ideao@gmail.com>
2022-07-25 12:51:01 -0300

    Remove auth.User import at test views

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,5 +1,4 @@
 from django.test import TestCase, RequestFactory
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse
 from ..views import UserLoginView
