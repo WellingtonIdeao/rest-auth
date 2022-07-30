548cbec6a71324e43b9105ec219be094b277c2ce
Wellington Ideão <wellington.ideao@gmail.com>
2022-07-30 20:49:57 -0300

    Added django-cors-headers application

--- django_rest_auth/settings/base.py
+++ django_rest_auth/settings/base.py
@@ -27,9 +27,11 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +120,8 @@ REST_FRAMEWORK = {
     ],
     'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
+
+# Django cors headers config
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+]
